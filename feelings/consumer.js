ef12baca9b52611654f6d773a6367f6782d4718c
Adrienol <delasalle.sio.olivon.a@gmail.com>
2017-05-04 11:29:15 +0200

    pour le test

--- feelings/consumer.js
+++ feelings/consumer.js
@@ -1,5 +1,7 @@
 var bus = require('servicebus').bus({ url: "amqp://192.168.99.100" });
+var reader = require('./reader.js');
 
-bus.listen('tweeter', function (event) {
-  console.log(event);
+bus.listen('tweeter', function (tweet) {
+  var sentiment = reader(tweet.text);
+  console.log(sentiment);
 });
