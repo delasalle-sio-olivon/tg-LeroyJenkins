13fb20c2105fc2bfda768d5da6411d89399dbc98
Adrienol <delasalle.sio.olivon.a@gmail.com>
2017-05-04 17:18:58 +0200

    oups

--- feelings/reader.js
+++ feelings/reader.js
@@ -1,6 +1,6 @@
 var sentiment = require('sentiment');
 var sentimentFr = require('sentiment-french');
-module.exports = function (text){
+module.exports = function (text, lang){
 
     if(lang === "fra"){
         var result = sentimentFr(text);
